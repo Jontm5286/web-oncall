eaeb5b268b87a0d4abe070180fec65472ddb044b
Jontm5286 <johantavarez89@gmail.com>
2025-09-15 15:33:27 -0400

    feat: agregar adaptador de Vercel para despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@astrojs/tailwind';
 import icon from 'astro-icon';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: { enabled: false },
   integrations: [tailwindcss(), icon()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
